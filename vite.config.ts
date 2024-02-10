f5f75abd6d093e9444add94a575bdfa2f199fc76
crimson <2272650008@qq.com>
2024-02-10 14:12:27 +0800

    feat: element-plus引入

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
+// 以下三项引入是为配置Element-plus自动按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // vite官网
 // https://vitejs.dev/config/
 
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		AutoImport({
+			resolvers: [ElementPlusResolver()]
+		}),
+		Components({
+			resolvers: [ElementPlusResolver()]
+		})
+	],
 	resolve: {
 		alias: {
 			'@': resolve('src')
