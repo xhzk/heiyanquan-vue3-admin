4173afed54f00cf6c078004b8292eb029a2ffc57
crimson <2272650008@qq.com>
2024-02-10 14:13:06 +0800

    feat: css原子化

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { setupRouter } from '@/router'
+
+// 一定要在main.ts中导入tailwind.css，防止vite每次hmr都会请求src/style/index.scss整体css文件导致热更新慢的问题
+import './style/tailwind.css'
 
 const app = createApp(App)
-import { setupRouter } from '@/router'
 
 const setupApp = async () => {
 	// 创建路由
