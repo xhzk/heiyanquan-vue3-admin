7a9e1e9af36379800289376ea1fac0e4fdeaae6c
crimson <2272650008@qq.com>
2024-02-22 09:44:16 +0800

    revert: 撤销svg组件

--- build/plugins.ts
+++ build/plugins.ts
@@ -1,20 +1,14 @@
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
-// svg plugin
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 // 以下三项引入是为配置Element-plus自动按需导入
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import { resolve } from 'path'
 
 export function getPluginsList() {
 	return [
 		vue(),
 		svgLoader(),
-		createSvgIconsPlugin({
-			iconDirs: [resolve(__dirname, '../src/assets/svg')]
-		}),
 		AutoImport({
 			resolvers: [ElementPlusResolver()]
 		}),
--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,6 @@ import { injectResponsiveStorage } from '@/utils/responsive'
 
 const app = createApp(App)
 
-// svg相关
-import 'virtual:svg-icons-register'
-import SvgIcon from './components/svgIcon/index.vue'
-
 // 引入重置样式
 import './style/reset.scss'
 // 一定要在main.ts中导入tailwind.css，防止vite每次hmr都会请求src/style/index.scss整体css文件导致热更新慢的问题
@@ -28,7 +24,6 @@ getServerConfig(app).then(async (config) => {
 	setupRouter(app)
 	injectResponsiveStorage(app, config)
 	setupStore(app)
-	app.component('svg-icon', SvgIcon)
 	app.use(MotionPlugin)
 	app.mount('#app')
 })
