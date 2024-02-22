a023335b27ca5c1767249a2249adcac7fb592d65
crimson <2272650008@qq.com>
2024-02-23 06:20:26 +0800

    feat: 顶部架子

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,6 +2,9 @@
 import '@/components/ReIcon/src/offlineIcon'
 import { ref } from 'vue'
 import SiderBar from './components/sidebar/index.vue'
+import BreadCrumb from './components/head/breadCrumb.vue'
+import ExtraIcon from './components/head/extraIcon.vue'
+import DropMenu from './components/head/dropMenu.vue'
 import { useNav } from './hooks/useNav'
 const { isCollapse } = useNav()
 const isCollapseRef = ref(isCollapse)
@@ -14,13 +17,17 @@ const isCollapseRef = ref(isCollapse)
 			<SiderBar :style="{ width: isCollapseRef ? '64px' : '200px' }" />
 		</div>
 		<div class="main-container">
-			<div class="head"></div>
+			<div class="head">
+				<ExtraIcon />
+				<BreadCrumb />
+				<DropMenu class="dropmenu" />
+			</div>
 			<div class="main">
 				<router-view />
 			</div>
-			<div class="footerbox">
+			<!-- <div class="footerbox">
 				<div>黑眼圈脚手架 ©2024 Created by 黑眼圈</div>
-			</div>
+			</div> -->
 		</div>
 	</div>
 </template>
@@ -43,8 +50,13 @@ const isCollapseRef = ref(isCollapse)
 }
 .head {
 	height: 75px; //与logo同高
+	padding: 0 20px;
 	display: flex;
 	border-bottom: #ebf0f6 2px solid;
+	align-items: center;
+}
+.dropmenu {
+	margin-left: auto;
 }
 .main {
 	flex: 1;
