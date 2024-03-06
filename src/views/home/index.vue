e718d4e1ed351eb5f8bc15c02f66bf0052e3781a
crimson <2272650008@qq.com>
2024-03-06 14:00:38 +0800

    24.3.6

--- src/views/home/index.vue
+++
@@ -1,4 +0,0 @@
-<script lang="ts" setup></script>
-<template>主页</template>
-
-<style lang="scss" scoped></style>
--- src/views/login/utils/static.ts
+++
@@ -1,4 +0,0 @@
-import bg from '@/assets/login/bg.png'
-import picture from '@/assets/login/pic.svg?component'
-
-export { bg, picture }
